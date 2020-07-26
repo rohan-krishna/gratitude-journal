15a733f93d8f51a68fa6c10dd671af1f3f802196
rohan-krishna <phonemg30993@gmail.com>
2020-07-26 13:19:37 +0530

    Settings module, split for development and production.

--- journalapp/settings/common.py
+++ journalapp/settings/common.py
@@ -19,13 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f^4a*3@c8%3n!389xy5sznfmp-j=-2=bd%^yho_#rtp&0_$=z@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
---
+++ journalapp/settings/development.py
@@ -0,0 +1,9 @@
+from journalapp.settings.common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'f^4a*3@c8%3n!389xy5sznfmp-j=-2=bd%^yho_#rtp&0_$=z@'
